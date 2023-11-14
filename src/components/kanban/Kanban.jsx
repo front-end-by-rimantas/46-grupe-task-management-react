561620bfb665d9a192f818ed4736cc16ea891403
Rimantas <belauzas@gmail.com>
2023-11-14 09:39:06 +0200

    CSS modules

--- src/components/kanban/Kanban.jsx
+++ src/components/kanban/Kanban.jsx
@@ -1,41 +1,49 @@
-import './Kanban.css';
+import style from './Kanban.module.css';
 
 export function Kanban() {
     return (
-        <section id="kanban" className="todo" style={{ gridTemplateColumns: 'repeat(4, 1fr)' }}>
-            <div className="column">
-                <h2 className="title">Backlog</h2>
-                <ul className="task-list">
-                    <li id="task_1" className="task-card">
-                        <div className="task-actions">
-                            <button className="fa fa-trash"></button>
+        <section id="kanban" className={style.todo} style={{ gridTemplateColumns: 'repeat(4, 1fr)' }}>
+            <div className={style.column}>
+                <h2 className={style.title + ' normal underline'}>Backlog</h2>
+                <ul className={style.taskList}>
+                    <li id="task_1" className={style.taskCard}>
+                        <div className={style.taskActions}>
+                            <button className={style.btn}>Delete</button>
+                            {/* <button className="fa fa-trash"></button> */}
                         </div>
-                        <div className="task-title">aaa</div>
-                        <div className="task-desc">aaa</div>
-                        <div className="task-tags"><div className="tag" style={{ color: '#333' }}>aaa</div></div>
-                        <div className="task-deadline">aaa</div>
+                        <div className={style.taskTitle}>aaa</div>
+                        <div className={style.taskDesc}>aaa</div>
+                        <div className={style.taskTags}>
+                            <div className={style.tag} style={{ color: '#333' }}>aaa</div>
+                        </div>
+                        <div className={style.taskDeadline}>aaa</div>
                     </li>
-                    <li id="task_2" className="task-card">
-                        <div className="task-actions">
-                            <button className="fa fa-trash"></button>
+                    <li id="task_2" className={style.taskCard}>
+                        <div className={style.taskActions}>
+                            <button className={style.btn}>Delete</button>
+                            {/* <button className="fa fa-trash"></button> */}
                         </div>
-                        <div className="task-title">bbb</div>
-                        <div className="task-desc">bbb</div>
-                        <div className="task-tags"><div className="tag" style={{ color: '#333' }}>bbb</div></div>
-                        <div className="task-deadline">bbb</div>
-                    </li></ul>
+                        <div className={style.taskTitle}>bbb</div>
+                        <div className={style.taskDesc}>bbb</div>
+                        <div className={style.taskTags}>
+                            <div className={style.tag} style={{ color: '#333' }}>bbb</div>
+                        </div>
+                        <div className={style.taskDeadline}>bbb</div>
+                    </li>
+                </ul>
             </div>
-            <div className="column">
-                <h2 className="title">Todo</h2>
-                <ul className="task-list"></ul>
+            <div className={style.column}>
+                <h2 className={style.title + ' normal underline'}>Todo</h2>
+                <ul className={style.taskList}></ul>
             </div>
-            <div className="column">
-                <h2 className="title">In progress</h2>
-                <ul className="task-list"></ul>
+            <div className={style.column}>
+                <h2 className={style.title + ' normal underline'}>In progress</h2>
+                <ul className={style.taskList}></ul>
             </div>
-            <div className="column">
-                <h2 className="title">Done</h2>
-                <ul className="task-list"></ul>
-            </div></section>
+            <div className={style.column}>
+                <h2 className={style.title + ' normal underline'}>Done</h2>
+                <ul className={style.taskList}></ul>
+            </div>
+        </section>
     );
 }
